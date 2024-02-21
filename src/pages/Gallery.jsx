c2674ff9a2224340778f8bf941293f878b2bed2f
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2024-02-21 17:43:47 +0530

    style: made card and those 4 buttons responsive

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,7 +3,7 @@ import SwiperGallery from '../components/SwiperGallery'
 
 const Gallery = () => {
   return (
-    <div id="gallery-container" className="h-[100vh]">
+    <div id="gallery-container" className="">
         <h1 className='m-5 text-pink text-4xl font-semibold'>GALLERY</h1>
         <SwiperGallery/>
     </div>
