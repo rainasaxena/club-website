cb018ff16071eb65ecb4ff8e4528a0e3aa64fad5
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2024-03-21 13:30:56 +0530

    did some styling

--- src/pages/JoinUs.jsx
+++ src/pages/JoinUs.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const JoinUs = () => {
   return (
     <div id="join-us" className='flex flex-col items-center justify-center m-20'>
-              <div className=" w-96 bg-teal rounded-lg p-10 flex flex-col gap-5">
-                <h1 className='text-4xl font-bold'>JOIN US</h1>
-                <div className="h-9 text-2xl font-bold bg-grey rounded-lg "><a href="">Fill the form</a></div>
-              </div>
+      <div className=" w-96 bg-teal rounded-lg p-10 flex flex-col gap-5">
+        <h1 className='text-4xl font-bold'>JOIN US</h1>
+        <div className="h-9 text-2xl font-bold bg-grey rounded-lg "><a href="">Fill the form</a></div>
+      </div>
 
     </div>
   )
